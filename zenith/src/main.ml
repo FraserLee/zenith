84080b57d15469dd790309c1690f177336d27899
Fraser <tomatoesneverlie@gmail.com>
2023-09-21 02:33:50 -0400

    simplify print function

--- zenith/src/main.ml
+++ zenith/src/main.ml
@@ -2,7 +2,7 @@ open Ast
 open Lexing
 open Printf
 
-let parse_with_error lexbuf =
+let parse lexbuf =
     try Parser.main Lexer.token lexbuf with
     | Parser.Error ->
         let pos = lexbuf.lex_curr_p in
@@ -17,32 +17,26 @@ let rec evaluate = function
     | Div(e1, e2) -> evaluate e1 / evaluate e2
 
 let print ast =
+
     let rec print' ast depth =
         let tab = String.make depth ' ' in
         match ast with
         | Int(n) -> printf "%sInt(%d)\n" tab n
-        | Add(e1, e2) -> printf "%sAdd(\n" tab; 
-                         print' e1 (depth + 1); 
-                         print' e2 (depth + 1); 
-                         printf "%s)\n" tab
-        | Sub(e1, e2) -> printf "%sSub(\n" tab; 
-                         print' e1 (depth + 1); 
-                         print' e2 (depth + 1);
-                         printf "%s)\n" tab
-        | Mul(e1, e2) -> printf "%sMul(\n" tab;
-                         print' e1 (depth + 1);
-                         print' e2 (depth + 1);
-                         printf "%s)\n" tab
-        | Div(e1, e2) -> printf "%sDiv(\n" tab;
-                         print' e1 (depth + 1);
-                         print' e2 (depth + 1);
-                         printf "%s)\n" tab
+        | Add(e1, e2) | Sub(e1, e2) | Mul(e1, e2) | Div(e1, e2) 
+        -> let op = match ast with
+                    | Add _ -> "Add" | Sub _ -> "Sub" | Mul _ -> "Mul" | Div _ -> "Div"
+                    | _ -> failwith "unreachable"
+           in printf "%s%s(\n" tab op;
+              print' e1 (depth + 1);
+              print' e2 (depth + 1);
+              printf "%s)\n" tab
+
     in print' ast 0
 
 
 let () =
     let lexbuf = Lexing.from_channel stdin in
-    let ast = parse_with_error lexbuf in
+    let ast = parse lexbuf in
     print ast;
     printf "Result: %d\n" @@ evaluate ast
 
